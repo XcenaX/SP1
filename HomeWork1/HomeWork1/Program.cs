33331453c9647f0b4c90314215992c08d6075225
XcenaX <40425017+XcenaX@users.noreply.github.com>
2019-03-20 09:59:47 +0600

    Add files via upload

---
+++ HomeWork1/HomeWork1/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Console;
+
+namespace HomeWork1
+{
+    class Program
+    {
+        static void ShowInfoAboutProcess(Process process)
+        {
+            WriteLine(process.ProcessName + "\t\t\t" + process.Id + "\t\t\t" + process.BasePriority);            
+        }
+
+        static void Main(string[] args)
+        {
+            var processes = Process.GetProcesses();
+            WriteLine("Имя\t\t\tId\t\t\tПриоритет");
+            foreach(var process in processes)
+            {
+                ShowInfoAboutProcess(process);
+            }
+            WriteLine("\n\nНажмите любую кнопку . . .");
+            ReadKey();
+        }
+    }
+}
